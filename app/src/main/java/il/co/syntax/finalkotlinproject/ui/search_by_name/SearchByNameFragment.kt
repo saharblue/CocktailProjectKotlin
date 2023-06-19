ebae82c921700e5051641c4a56d7b81eb755c162
Sahar Blueshtein <blueshtein.sahar@post.runi.ac.il>
2023-06-19 12:19:15 +0300

    created SearchByNameFragment

--- app/src/main/java/il/co/syntax/finalkotlinproject/ui/search_by_name/SearchByNameFragment.kt
+++ app/src/main/java/il/co/syntax/finalkotlinproject/ui/search_by_name/SearchByNameFragment.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import il.co.syntax.finalkotlinproject.R
 import il.co.syntax.finalkotlinproject.databinding.CocktailDetailFragmentBinding
 import il.co.syntax.finalkotlinproject.databinding.SearchByNameFragmentBinding
+import il.co.syntax.finalkotlinproject.ui.all_cocktails.CocktailsAdapter
 import il.co.syntax.finalkotlinproject.ui.detailed_cocktail.DetailedCocktailViewModel
 import il.co.syntax.fullarchitectureretrofithiltkotlin.utils.autoCleared
 
 @AndroidEntryPoint
-class SearchByNameFragment : Fragment() {
+class SearchByNameFragment :  Fragment() {
 
     private var binding: SearchByNameFragmentBinding by autoCleared()
 
@@ -26,4 +30,19 @@ class SearchByNameFragment : Fragment() {
         binding = SearchByNameFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchButton.setOnClickListener {
+            onButtonClick(binding.searchCocktail.text.toString())
+        }
+    }
+
+     private fun onButtonClick(cocktailName: String){
+        findNavController().navigate(
+            R.id.action_allCocktailsFragment_to_detailedCocktailFragment2,
+            bundleOf("cocktailName" to cocktailName))
+    }
+
 }
