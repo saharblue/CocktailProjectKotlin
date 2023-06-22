c8ed090dfe4c3c926ea21de4e8a97eb52982e9db
Sahar Blueshtein <blueshtein.sahar@post.runi.ac.il>
2023-06-22 14:01:33 +0300

    added navbar icons

--- app/src/main/java/il/co/syntax/finalkotlinproject/data/remote_db/BaseDataSource.kt
+++ app/src/main/java/il/co/syntax/finalkotlinproject/data/remote_db/BaseDataSource.kt
@@ -1,5 +1,6 @@
 package il.co.syntax.finalkotlinproject.data.remote_db
 
+import android.util.Log
 import il.co.syntax.finalkotlinproject.utils.Resource
 import retrofit2.Response
 
@@ -7,12 +8,14 @@ abstract class BaseDataSource {
 
     protected suspend fun <T>
             getResult(call : suspend () -> Response<T>) : Resource<T> {
+        val TAG = "BaseDataSource"
 
         try {
             val result  = call()
             if(result.isSuccessful) {
                 val body = result.body()
                 if(body != null) return  Resource.success(body)
+                else Log.d(TAG, "getResult: body is null")
             }
             return Resource.error("Network call has failed for the following reason: " +
                     "${result.message()} ${result.code()}")
